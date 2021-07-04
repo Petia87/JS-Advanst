b0f27801b3757b4137e2764c200cf503d23a10a1
Petia Valchkova <peps@gmail.com>
2021-07-04 16:43:27 +0300

    01

---
+++ 01Syntax-Functions-and-Statements-Lab/05MathOperations.js
@@ -0,0 +1,17 @@
+function mathOperation(num1, num2, operation){
+    if(operation==='+'){
+        console.log(num1+num2)
+    }else if(operation==='-'){
+        console.log(num1-num2)        
+    }else if(operation==='*'){
+        console.log(num1*num2)
+    }else if(operation==='/'){
+        console.log(num1/num2)
+    }else if(operation==='%'){
+        console.log(num1 % num2)
+    }else if(operation==='**'){
+        console.log(num1**num2)
+    }    
+
+}
+mathOperation(1,2,"+")
