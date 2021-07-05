b10d4ba26b56e52933c7db66eaee928bc97709e2
Petia Valchkova <peps@gmail.com>
2021-07-05 09:45:58 +0300

    02

--- 04DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ 04DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById('text').value;
+  let naming = document.getElementById('naming-convention').value;
+
+
+  if (naming === 'Camel Case') {
+    resultSpan = '';
+    let arr = input.split(' ');
+    for (let index = 0; index < arr.length; index++) {
+      if (index == 0) {
+        resultSpan += arr[index].toLowerCase();
+      } else {
+        resultSpan += arr[index].charAt(0).toUpperCase() + arr[index].toLowerCase().slice(1);
+      }
+    }
+  } else if (naming === 'Pascal Case') {
+    resultSpan = '';
+    let arr = input.split(' ');
+    for (let index = 0; index < arr.length; index++) {
+      resultSpan += arr[index].charAt(0).toUpperCase() + arr[index].toLowerCase().slice(1);
+    }
+  } else {
+    resultSpan = 'Error!'
+  }
+
+  document.getElementById('result').textContent = resultSpan;
 }
