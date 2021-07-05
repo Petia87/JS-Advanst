09089d5f552965fe927bbfb0273724ba0cf5e56f
Petia Valchkova <peps@gmail.com>
2021-07-05 09:51:14 +0300

    03

--- 04DOM Introduction - Exercise/03. Accordion/accordion.js
+++ 04DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonObj = document.getElementsByClassName('button')[0];
+    let elementToManipulate=document.getElementById('extra');
+
+    if (buttonObj.textContent==='More') {
+        buttonObj.textContent='Less';
+        elementToManipulate.style.display='block';
+    }else{
+        buttonObj.textContent='More';
+        elementToManipulate.style.display='none';
+    }
 }
