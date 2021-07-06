d57c7eb5a59075b8b4d301af4b83352b1f354333
Petia Valchkova <peps@gmail.com>
2021-07-06 09:17:11 +0300

    06

--- 05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
+++ 05. JS-Advanced-DOM-Events-Lab-Resources/06. Dynamic-Validation/app.js
@@ -1,3 +1,20 @@
 function validate() {
-    console.log('TODO:...');
+        
+    const emailField=document.getElementById('email');
+
+    emailField.addEventListener('change', onChangeHandler);
+
+    function onChangeHandler(event) {
+
+        //Regex from Stackoverflow
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (re.test(String(event.target.value).toLowerCase())) {
+            //valid
+            event.target.className='';
+        }
+        else{
+            //invalid
+            event.target.className='error';
+        }       
+    }
 }
