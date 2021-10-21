35a59cb140b44313c549835a4f14ffa2cad0b672
Petia Valchkova <peps@gmail.com>
2021-10-21 12:58:28 +0300

    10

--- teleric_fundamentals/type/forloop/10.js
+++ teleric_fundamentals/type/forloop/10.js
@@ -2,7 +2,7 @@ let input = [6, 9,11,3,2,1,8]
 
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
-llet n = +gets();
+let n = +gets();
 let current
 let max=Number.MIN_SAFE_INTEGER
 
@@ -15,3 +15,4 @@ for (let i = 0; i < n; i++) {
         max=current
     }
 }
+print(max)
