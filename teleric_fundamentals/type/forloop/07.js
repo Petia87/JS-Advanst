ee71655073d9b0d26098e0e205cd8897a3a05aef
Petia Valchkova <peps@gmail.com>
2021-10-21 10:46:34 +0300

    07

---
+++ teleric_fundamentals/type/forloop/07.js
@@ -0,0 +1,23 @@
+let input = [3,2]
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+let n= +gets();
+let x=+gets()
+let result=1
+let res=0
+
+let sum=1
+
+//S=1 + 1.1/x.1 + 1.2/x.2 + 1.2.3/x.3 + …+ 1.2.3…N/x.N
+//sum +=Factoriel(i) /Math.Pow(x, I);
+for (let i = 1; i <=n; i++) {
+    result=result*i
+    let del=Math.pow(x,i)
+ res=result/del
+    sum+=res
+      
+}
+print(sum.toFixed(5))
+
+git@github.com:Petia87/react_basic.git
