1c23bdb5f11a82ba00f97bc9e893ceb8060e2cea
Petia Valchkova <peps@gmail.com>
2021-10-21 12:55:03 +0300

    09

--- teleric_fundamentals/type/forloop/09.js
+++ teleric_fundamentals/type/forloop/09.js
@@ -3,17 +3,17 @@ let input = [5,2,1,1,6,3]
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 let n= +gets();
-let sumOdd=0
-let sumEven=0
+let sumOdd=1
+let sumEven=1
 
 
-for (let i = 1; i <=n; i++) {
+for (let i = 0; i <n; i++) {
    let next=+gets()
    let current=next
-   if (i%2==0) {
-    sumOdd+=current
+   if (i%2!==0) {
+    sumOdd*=current
    } else {
-    sumEven+=current
+    sumEven*=current
    }
 }
 if (sumOdd!=sumEven) {
