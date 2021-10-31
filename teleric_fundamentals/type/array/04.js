9a0c373be504e9bd3e8b96c79393c0775c0fd743
Petia Valchkova <peps@gmail.com>
2021-10-31 19:45:16 +0200

    04

--- teleric_fundamentals/type/array/04.js
+++ teleric_fundamentals/type/array/04.js
@@ -1,14 +1,21 @@
-let input = ["5,3,2,1", "2"]
+/*2.	Common Elements
+Write a function, which prints common elements in two string arrays. Print all matches on separate lines. Compare the first array with the second array
+Input	Output
+['Hey', 'hello', 2, 4, 'Peter', 'e'],
+['Petar', 10, 'hey', 4, 'hello', '2']	hello
+4
+['S', 'o', 'f', 't', 'U', 'n', 'i', ' '],
+['s', 'o', 'c', 'i', 'a', 'l']	o 
+i
 
+*/
+let input = ["'Hey', 'hello', 2, 4, 'Peter', 'e'","['Petar', 10, 'hey', 4, 'hello', '2']"]
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
-let arr = gets().split(',')
-let n = +gets()
 
-let resultArr = [...arr]
 
-for (let i = 1; i <=n; i++) {
-    let element=arr.shift()
-    arr.push(element)
-}
-print(arr.join(" "))
+
+let arr = gets().split(",")
+let arr2=gets().split(",")
+arr3=[]
+arr.map((i)=>arr2.includes(i)?print(i):isEgual=false)
