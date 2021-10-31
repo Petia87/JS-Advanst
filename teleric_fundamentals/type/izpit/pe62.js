3f8d77bca390a0bd79dd63bbaa03248ef2ddbce1
Petia Valchkova <peps@gmail.com>
2021-10-31 23:35:40 +0200

    2,6

--- teleric_fundamentals/type/izpit/pe62.js
+++ teleric_fundamentals/type/izpit/pe62.js
@@ -9,62 +9,23 @@
  
  */
 
- let input = ["-5,-5,-5,-5,5"]
+let input = ["2,2,2,2,-20"]
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 let n = gets().split(",").map(Number)
-let min=0
-let max=0
-let prev=0
+let min = 0
+let max = 0
 let h = 0
-let sum1=0
-let sum2=0
-let h1 = 0
-let h2 = 0
-  for (let i = 0; i < n.length; i++) {
-      const el = +n[i];
-      
-      if (el>=0) {
-          sum1+=el
-          h+=el
-          if (prev>0) {
-              max+=el
-              h1=max
-          }else{
-              h1=max
-              max=0
-          }
-      } else{
-         h+=el
-         sum2+=el
-         if (prev<0) {
-            min+=el
-            h2=min
-        }else{
-           
-            min=0
-        }
-        prev=el
-      } 
+
+for (let i = 0; i < n.length; i++) {
+  const el = +n[i];
+  
+  h += el
+  if (h > max) {
+    max = h
+  } else if (h < min) {
+    min = h
   }
-  print(h)
-  print(sum1)
-  print(sum2)
-  print(max)
-  print(min)
-  print(h1)
-  print(h2)
-  //В случай, че се брои първата  точка от 0, 
-  /*if (sum1+sum2<0) {
-    print(`hmax=${sum1}`)
-    print(`hmin=${sum2+sum1}`)
-  } else if(sum1<0){
-    print(`hmax=${0}`)
-    print(`hmin=${sum2+sum1}`)
-  } else if(sum2>0){
-    print(`hmax=${sum1}`)
-    print(`hmin=${0}`)
-  }else if(sum1+sum2>0){
-    print(`hmax=${sum1}`)
-    print(`hmin=${0}`)
-  }*/
+}
+print(max)
+print(min)
