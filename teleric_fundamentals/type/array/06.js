995ec9509bf1339102b1b55681c06afcad30b0fa
Petia Valchkova <peps@gmail.com>
2021-10-31 19:45:29 +0200

    06

--- teleric_fundamentals/type/array/06.js
+++ teleric_fundamentals/type/array/06.js
@@ -1,17 +1,34 @@
-let input = ["1,2,2,2,2,18,3,12"]
-
+/*5.	Max Number
+Input	Output
+[1, 4, 3, 2]	4 3 2
+[14, 24, 3, 19, 15, 17]	24 19 17
+[41, 41, 34, 20]	41 34 20
+[27, 19, 42, 2, 13, 45, 48]	48
+Write a function to find all the top integers in an array. A top integer is an integer which is bigger than all the elements to its right. 
+Output
+Print all top integers on the console, separated by single space
+*/
+let input = ["27, 19, 42, 2, 13, 45, 48"]
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
-let arr = gets().split(',')
-let arr2=[]
+
+let arr = gets().split(",").map(Number)
+let max = []
+let isBigger = true
+let curr = 0
+let next = 0
 for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    if (!arr2.includes(element)) {
-        arr2.push(element)
+    curr = arr[i]
+    next = arr[i + 1]
+    if (curr < next) {
+        isBigger = false
+        max = []
+    } else {
+        isBigger = true
     }
-    
+    if (isBigger) {
+        max.push(curr)
+    }
+
 }
-
-
-
-print(arr2)
+print(max)
