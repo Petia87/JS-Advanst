20c289393b1a255d1b88c1d71f145ce1f4aee64c
Petia Valchkova <peps@gmail.com>
2021-10-31 19:46:01 +0200

    11

--- teleric_fundamentals/type/array/11.js
+++
@@ -1,8 +0,0 @@
-
-
-let input = ["0,15,30"]
-
-let print = this.print || console.log;
-let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
-let arr = gets().split(',').map(Number)
-arr.forEach(x=>print(x* 1.8 + 32))
