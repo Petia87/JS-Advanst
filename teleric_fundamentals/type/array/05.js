4f5a76b6f80ed053300c43ce049036feb56a43b5
Petia Valchkova <peps@gmail.com>
2021-10-31 19:45:23 +0200

    05

--- teleric_fundamentals/type/array/05.js
+++ teleric_fundamentals/type/array/05.js
@@ -1,56 +1,21 @@
-let input = ["3",
-"1,2,3,4,5",
-"1,2,8,9,9",
-"-4,-3,-2,-1,0"]
-
+/*4.	Array Rotation
+Input	Output
+[51, 47, 32, 61, 21], 2	32 61 21 51 47
+[32, 21, 61, 1], 4	32 21 61 1
+[2, 4, 15, 31], 5	4 15 31 2
+Write a function that receives an array and number of rotations you have to perform (first element goes at the end). 
+Output Print the resulting array elements separated my single space
+*/
+let input = ["51, 47, 32, 61, 21","2"]
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
-let n = +gets()
-let isSorted = true
-//let el;
-//let previous
 
-for (let i = 1; i <= n; i++) {
-  let arr = gets().split(',').map(Number)
-  //let arr2=[...arr].sort((a,b)=>a-b)
-  
-  for (let j = 0; j < arr.length; j++) {
-   if (j===0) {
-     continue
-   }
-    let el = +arr[j];
-    let prev=+arr[j-1]
-      if (el<prev) {
-       isSorted = false
-        print(false)
-        break        
-      }
-    
-  }
+let arr = gets().split(",").map(Number)
+let num= gets()
 
-  if (isSorted) {
-    print(true)
-  } 
-  isSorted = true
+for (let i = 0; i <num; i++) {
+let first=arr.shift()
+arr.push(first)
+
 }
-
-
-//print(arr)
-
-/*for (let i = 1; i <= n; i++) {
-    let arr = gets().split(',')
-    let arr2=[...arr]
-    arr2.sort((a,b)=>a-b)
-    arr.map((element, index) => {
-       if (Number(element)===Number(arr2[index])) {
-        isTrue
-       } else {
-        isTrue=false
-       }
-    })
-      if (isTrue) {
-          print(true)
-      } else {
-        print(false)
-      }
-}*/
+print(arr.join(' '))
