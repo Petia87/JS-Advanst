3461616b06d6cf7af9541ef0b40977046910af08
Petia Valchkova <peps@gmail.com>
2021-10-31 19:44:54 +0200

    01

--- teleric_fundamentals/type/array/01.js
+++ teleric_fundamentals/type/array/01.js
@@ -1,9 +1,17 @@
-let input = ["2, 3, 1, 5, 6"]
-
+/*4.	Reverse an Array of Strings
+Write a program which receives an array of strings (space separated values). Your task is to reverse it and print its elements. Swap elements. 
+*/
+let input = ["'a', 'b', 'c', 'd', 'e'"]
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
-let n = gets().split(',').map(Number)
+
+let arr = [...gets()]
+let lastIndex=arr.length-1
+for (let i = 0; i < arr.length/2; i++) {
+let temp=arr[i]
+arr[i]=arr[lastIndex-i]
+arr[lastIndex-i]=temp
 
 
-let sorted=n.sort((a,b)=>b-a)
-print(sorted)
+}
+print(arr.join(''))
